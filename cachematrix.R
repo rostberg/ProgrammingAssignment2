05feb58ed6c638855b480d864ebeca4a37e8985e
rostberg <mattostberg@yahoo.com>
2017-08-13 14:36:49 -0600

    My stupid solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matt Ostberg 8/13/2017
+## Programming Assignment 2
+## write functions to create matrix with cache
 
-## Write a short comment describing this function
+## This creates a matrix with cache.
+## It's a cargo-cult implementation (I took the example function and modified it)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This also borrows heavily from the example and substitutes solve() for mean()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  if(det(data) == 0) {print("Singular matrix, no inverse.")}
+  else { 
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+  }
 }
